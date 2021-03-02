e4e05912c7d5890ca410ce65543bafb88b616fdb
gmodena <gmodena@wikimedia.org>
2021-03-02 21:03:21 +0100

    Store artifacts and metrics by run identifier

--- publish.sh
+++ publish.sh
@@ -10,10 +10,14 @@
 # 
 # Where <username> is the user currently running the publish.sh script.
 #
-# Production datasets will be exported locally, in tsv format, under ./imagerec_prod_${snapshot}.
+# Production datasets will be exported locally, in tsv format, under runs/<run_id>/imagerec_prod_${snapshot}.
 #
 # Usage: ./publish.sh <snapshot>
 # Example: ./publish.sh 2021-01-25
+
+snapshot=$1
+
+run_id=$(cat /proc/sys/kernel/random/uuid)
 
 # Target wikis to train ImageMatching on
 wikis="enwiki arwiki kowiki cswiki viwiki frwiki fawiki ptwiki ruwiki trwiki plwiki hewiki svwiki ukwiki huwiki hywiki srwiki euwiki arzwiki cebwiki dewiki bnwiki"
@@ -23,13 +27,21 @@ monthly_snapshot=$(echo ${snapshot} | awk -F'-' '{print $1"-"$2}')
 username=$(whoami)
 
 # Path were raw dataset (Jupyter algo output) will be stored
-algo_outputdir=Output
+algo_outputdir=${run_id}/Output
 
-make venv 
+# Path on the local filesystem where production datasets will be stored.
+outputdir=${run_id}/imagerec_prod_${snapshot}
+
 source venv/bin/activate
+
+mkdir -p $(pwd)/runs/${run_id}/
+metrics_dir=$(pwd)/runs/${run_id}/metrics
+mkdir -p $metrics_dir
+echo "Starting training run ${run_id} for snapshot=$snapshot. Model artifacts will be collected under
+$(pwd)/runs/${run_id}"
+
 # TODO(gmodena, 2021-02-02): 
-# Passing one wiki at a time to get a feeling for runtime deltas (to some degree, we could
-get this info from parsing hdfs snapshots).
+# Passing one wiki at a time to get a feeling for runtime deltas (to some degree, we could get this info from parsing hdfs snapshots).
 # We could pass the whole list at algorunner.py at once,
 # and have the pipeline run on a single (long running) spark job. Instead, here we
 # are spinning up one spark cluster per wiki. This needs checking with AE, in order
@@ -40,7 +52,8 @@ for wiki in ${wikis}; do
 	STARTIME=${SECONDS}
 	python algorunner.py ${snapshot} ${wiki} ${algo_outputdir}
 	ENDTIME=${SECONDS}
-	echo "metrics.algorunner.${wiki}.${snapshot}.seconds,$(($ENDTIME - $STARTTIME))"
+	metric_name=${metrics.algorunner.${wiki}.${snapshot}.seconds}
+	echo "${metric_name},$(($ENDTIME - $STARTTIME))" >> ${metrics_dir}/${metric_name}
 	
 
 	# 2. Upload to HDFS
@@ -51,7 +64,8 @@ for wiki in ${wikis}; do
 
 	hadoop fs -copyFromLocal ${algo_outputdir}/${wiki}_${snapshot}_wd_image_candidates.tsv imagerec/data/wiki_db=${wiki}/snapshot=${monthly_snapshot}/
 	ENDTIME=${SECONDS}
-	echo "metrics.hdfs.copyrawdata.${wiki}.${snapshot}.seconds,$(($ENDTIME - $STARTTIME))"
+	metric_name=metrics.hdfs.copyrawdata.${wiki}.${snapshot}.seconds
+	echo "${metric_name},$(($ENDTIME - $STARTTIME))" >> ${metrics_dir}/${metric_name}
 
 	# 3. Update hive external table metadata
 	echo "Updating Hive medatada for ${wiki}" 
@@ -59,33 +73,37 @@ for wiki in ${wikis}; do
 	hive -hiveconf username=${username} -f ddl/external_imagerec.hql
 done
 	ENDTIME=${SECONDS}
-	echo "metrics.hive.imagerec.${wiki}.${snapshot}.seconds,$(($ENDTIME - $STARTTIME))"
+	metric_name=metrics.hive.imagerec.${wiki}.${snapshot}.seconds
+	echo "${metric_name},$(($ENDTIME - $STARTTIME))" >> ${metrics_dir}/${metric_name}
 # 4. Submit the Spark production data ETL
 echo "Generating production data"
 hadoop fs -rm -r imagerec_prod/data/
 
 ## Generate spark config
-spark_config=conf/regular.spark.properties
+spark_config=$runs/$run_id/regular.spark.properties
 cat conf/spark.properties.template /usr/lib/spark2/conf/spark-defaults.conf > ${spark_config}
 
 STARTIME=${SECONDS}
 spark2-submit --properties-file ${spark_config} etl/transform.py ${snapshot} imagerec/data/ imagerec_prod/data/
 ENDTIME=${SECONDS}
-echo "metrics.etl.transfrom.${snapshot}.seconds,$(($ENDTIME - $STARTTIME))"
+metric_name=metrics.etl.transfrom.${snapshot}.second
+echo "${metric_name},$(($ENDTIME - $STARTTIME))" >> ${metrics_dir}/${metric_name}
 
 
 # 5. Update hive external table metadata (production)
 STARTIME=${SECONDS}
 hive -hiveconf username=${username} -f ddl/external_imagerec_prod.hql
 ENDTIME=${SECONDS}
-echo "hive.imagerec_prod.${snapshot}    $(($ENDTIME - $STARTTIME))"
+metric_name=hive.imagerec_prod.${snapshot}
+echo "hive.imagerec_prod.${snapshot},$(($ENDTIME - $STARTTIME))" >> ${metrics_dir}/${metric_name}
 
 # 6. Export production datasets
 STARTIME=${SECONDS}
-mkdir imagerec_prod_${snapshot}/
+mkdir ${outputdir}
 for wiki in ${wikis}; do
-	hive -hiveconf username=${username} -hiveconf wiki=${wiki} -f export_prod_data.hql >  imagerec_prod/prod-${wiki}-${snapshot}-wd_image_candidates.tsv
+	hive -hiveconf username=${username} -hiveconf wiki=${wiki} -f export_prod_data.hql > ${outputdir}/prod-${wiki}-${snapshot}-wd_image_candidates.tsv
 done
 ENDTIME=${SECONDS}
-echo "Datasets are available at $(pwd)/imagerec_prod_${snapshot}/"
-echo "metrics.etl.export_prod_data.${snapshot}.seconds,$(($ENDTIME - $STARTTIME))"
+echo "Datasets are available at $outputdir/imagerec_prod_${snapshot}/"
+metric_name=metrics.etl.export_prod_data.${snapshot}.seconds
+echo "${metric_name},$(($ENDTIME - $STARTTIME))" >> ${metrics_dir}/${metric_name}
