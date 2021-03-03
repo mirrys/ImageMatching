3e89ba6d784662e6c5209963d0f02c3e0aaa3613
gmodena <gmodena@wikimedia.org>
2021-03-03 22:15:49 +0100

    Add test for confidence mapping

--- conftest.py
+++ conftest.py
@@ -22,8 +22,21 @@ def raw_data(spark_session):
                 "Some page with no suggestion",
                 None,
                 "arwiki",
-                "2020-12"
-            )
+                "2020-12",
+            ),
+            (
+                "2",
+                "Q66666",
+                "523523",
+                "Some page with 3 suggestions",
+                '['
+                '   {"image": "image2.jpg", "rating": 2.0, "note": "image was found in the following Wikis: ruwiki"}, '
+                    '{"image": "image3.jpg", "rating": 1, "note": "image was in the Wikidata item"}, '
+                    '{"image": "image4.jpg", "rating": 3.0, "note": "image was found in the Commons category linked in the Wikidata item"}'
+                ']',
+                "enwiki",
+                "2020-12",
+            ),
         ],
         RawDataset.schema,
     )
--- tests/test_etl.py
+++ tests/test_etl.py
@@ -1,8 +1,10 @@
 from etl.transform import RawDataset, ImageRecommendation
 
+from pyspark.sql import functions as F
+
 
 def test_etl(raw_data):
-    assert raw_data.count() == 2
+    assert raw_data.count() == 3
 
     ddf = ImageRecommendation(raw_data).transform()
     assert (
@@ -21,5 +23,27 @@ def test_etl(raw_data):
         == 0
     )
 
-    expected_num_records = 2
+    expected_num_records = 5
     assert ddf.count() == expected_num_records
+
+    expected_confidence = {"wikipedia": "medium", "commons": "low", "wikidata": "high"}
+    for source in expected_confidence:
+        ddf.where(F.col("source") == source).select(
+            "confidence_rating"
+        ).distinct().collect()
+        rows = (
+            ddf.where(F.col("source") == source)
+            .select("confidence_rating")
+            .distinct()
+            .collect()
+        )
+        assert len(rows) == 1
+        assert rows[0]["confidence_rating"] == expected_confidence[source]
+
+    # Unillustrated articles with no recommendation have no confidence rating
+    assert (
+        ddf.where(F.col("source") == "null")
+        .where(F.col("confidence_rating") != "null")
+        .count()
+        == 0
+    )
