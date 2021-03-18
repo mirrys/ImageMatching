995e9fdbd132b2e4c36955095a97c1d6524148c3
gmodena <gmodena@wikimedia.org>
2021-03-18 12:11:25 +0100

    Add placeholder instanceof column in mocks

--- conftest.py
+++ conftest.py
@@ -12,6 +12,7 @@ def raw_data(spark_session):
                 "44444",
                 "Some page with suggestions",
                 '[{"image": "image1.jpg", "rating": 2.0, "note": "image was found in the following Wikis: ruwiki"}]',
+                "",
                 "arwiki",
                 "2020-12",
             ),
@@ -21,6 +22,7 @@ def raw_data(spark_session):
                 "55555",
                 "Some page with no suggestion",
                 None,
+                "",
                 "arwiki",
                 "2020-12",
             ),
@@ -34,6 +36,7 @@ def raw_data(spark_session):
                     '{"image": "image3.jpg", "rating": 1, "note": "image was in the Wikidata item"}, '
                     '{"image": "image4.jpg", "rating": 3.0, "note": "image was found in the Commons category linked in the Wikidata item"}'
                 ']',
+                "",
                 "enwiki",
                 "2020-12",
             ),
