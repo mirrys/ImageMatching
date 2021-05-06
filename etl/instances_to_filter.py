1f3326ae41739e93b71b7d7ffff7f36f49e8b243
Clara <candrewwani@gmail.com>
2021-05-06 15:55:20 -0400

    Add recurrent time frame to instances_to_filter list (#25)
    * Add recurrent time frame to instances_to_filter list
    
    * Add instance of century leap year, family name, and name to filter list

--- etl/instances_to_filter.py
+++ etl/instances_to_filter.py
@@ -4,6 +4,10 @@ from enum import Enum
 class InstancesToFilter(Enum):
     YEAR = "Q577"
     CALENDARYEAR = "Q3186692"
+    RECURRENTTIMEFRAME = "Q14795564"
+    CENTURYLEAPYEAR = "Q3311614"
+    FAMILYNAME = "Q101352"
+    NAME = "Q82799"
     DISAMBIGUATION = "Q4167410"
     LIST = "Q13406463"
 
